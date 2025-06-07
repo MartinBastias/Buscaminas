d71d7aa7331b68300fe9f0d0090301acf7ed6b9e
MartinBastias <mbastias2005@gmail.com>
2025-06-07 18:58:14 -0400

    filas columnas y bombas

--- #tarea semestral.py
+++ #tarea semestral.py
@@ -1,7 +1,23 @@
 #tarea semestral
-n = int(input('Ingrese cantidad de pepes:'))
-for x in range(n):
-    print("elpepesaikonigga")
+import random
 
-print("no mas pepes")
-print("un ultimo pepe")
+#tamaño tablero
+def leeValida(m, M):
+    f = int(input("Ingresa cantidad de filas: "))
+    while not m <= f <= M:
+        f = int(input(f"Reingresa la cantidad de filas entre {m} y {M}: "))
+    c = int(input("Ingresa cantidad de columnas: "))
+    while not m <= c <= M:
+        c = int(input(f"Reingresa la cantidad de columnas entre {m} y {M}: "))
+    return f,c
+
+#cantidad de bombas aleatorias entre 1 y f,c / 4
+def generarBombas():
+    return random.randint(1, int((filas*columnas)/4)) 
+
+#generar tablero
+
+
+
+filas, columnas = leeValida(3, 15)
+print(generarBombas())
