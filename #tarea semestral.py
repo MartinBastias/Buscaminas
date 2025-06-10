4c93635a32596893ca49fa823d85df69a3e12dd4
MartinBastias <mbastias2005@gmail.com>
2025-06-10 01:59:15 -0400

    bombas alrededor, ganar, perder, 2 tableros

--- #tarea semestral.py
+++ #tarea semestral.py
@@ -1,6 +1,11 @@
 #tarea semestral
 import random
 
+
+coordjug = [0, 0]
+posicionb = []
+tablero = []
+tablerovisual = []
 #tamaño tablero
 def leeValida(m, M):
     f = int(input("Ingresa cantidad de filas: "))
@@ -17,19 +22,56 @@ def generarCBombas():
 
 #generar tablero
 filas, columnas = leeValida(3, 15)
-tablero = []
+
 for i in range (filas):
     tablero.append([])
 for i in tablero:
     for x in range(columnas):
-        i.append(".")
+        i.append("#")
 
 #generar posicion de bobmbas
 def pb():
     return  random.randint(1, filas), random.randint(1, columnas) 
-posicionb = []
+
+def perderosguir():
+    if tablero[coordjug[0]-1][coordjug[1]-1] == "b":
+        return False
+    
+def ganar():
+        c = 0
+        for filast in tablero:
+            if "#" not in filast: # filas del ciclo for
+                c +=1
+        if c == filas:  #filas del tablero en general
+            return False
+def bombasalrededor():
+    b = 0
+    alrededor = [(-1, -1), (-1, 0), (-1, 1),
+                 (0, -1),           (0, 1),
+                 (1, -1), (1, 0), (1, 1)]
+    for i in alrededor:
+        rf = (coordjug[0]-1) + i[0] # revisar filas
+        rc = (coordjug[1]-1) + i[1] # columnas
+        if (0 <= rf <= filas-1) and (0 <= rc <= columnas-1): # por temas de com ose lee la lista se debe restar 1 al rango de posiciones
+            if tablero[rf][rc] == "b":
+                b +=1
+    return b
+
+def desplegartablero():
+    for posicion in tablero:
+        f=""
+        for i in posicion:
+            f+=(str(i).replace("b", "#"))
+        print(f)
+def desplegartablerobombas():
+    for posicion in tablero:
+            f=""
+            for i in posicion:
+                f+=(str(i))
+            print(f) 
 
 cb = generarCBombas()
+
 for i in range (cb):
     posicionb.append(pb())
     for p in posicionb:
@@ -39,5 +81,23 @@ for i in range (cb):
 #print tablero codigo
 print(cb)
 print(posicionb)
-for i in tablero:
-    print(i)
+
+
+while True:
+    desplegartablero()
+    fj = int(input("Ingresa la fila: "))
+    cj = int(input("Ingresa la columna: "))
+    coordjug[0] = fj
+    coordjug[1] = cj
+    
+    
+    if perderosguir() == False:
+        desplegartablerobombas()
+        print("perdiste, gana el pc ! ! ! ")
+        break
+
+    tablero[coordjug[0]-1][coordjug[1]-1] = bombasalrededor() # ssta linea hay que dejarla si o si despues de verificar si se pierde
+    if ganar() == False:
+        desplegartablero()
+        print("ganaste ! ! ! elpepe elpepe i hate ")
+        break
